9b3b0e40247dbdb5998979d39984be2fad929ba2
mwong068 <mwong068@gmail.com>
2020-01-23 09:51:24 -0800

    added full crud functionality to hikingtrails

--- app/controllers/hikingtrails_controller.rb
+++ app/controllers/hikingtrails_controller.rb
@@ -1,4 +1,6 @@
 class HikingtrailsController < ApplicationController
+  before_action :find_trail, only: [:show, :create, :update, :destroy]
+
   def index
     hikingtrails = Hikingtrail.all
     render json: hikingtrails
@@ -9,4 +11,29 @@ class HikingtrailsController < ApplicationController
     hikingtrail = Hikingtrail.find_by(id: params[:id])
     render json: hikingtrail
   end
+
+  def create
+    @hikingtrail = Hikingtrail.create_or_find_by(hikingtrail_params)
+    render json: @hikingtrail
+  end
+
+  def update
+      @hikingtrail.update(hikingtrail_params)
+      render json: @hikingtrail
+  end
+
+  def destroy
+      @hikingtrail.delete
+  end
+
+  private
+
+  def find_trail
+      @hikingtrail = Hikingtrail.find_by(id: params[:id])
+  end
+
+  def hikingtrail_params
+      params.require(:hikingtrail).permit(:hikingtrailname, :name, :email, :password)
+  end
 end
+
